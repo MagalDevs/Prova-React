d957dba43be9dec5c1ab3b13baec3949aff883c2
MagalDevs <danielgalfla12@gmail.com>
2024-11-18 19:47:02 -0300

    Footer sendo feito

---
+++ lojinha/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header>
+        <h1>IndaiaSports</h1>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>Produtos</li>
+            <li>Contato</li>
+          </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
