a77112764299f5f945b8217ab56392d6a9100d49
Gabriel Ribeiro <146293875+gmribeiro@users.noreply.github.com>
2024-11-19 20:36:05 -0300

    footer

--- lojinha/src/main.jsx
+++ lojinha/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import Header from './components/Header/Header'
 import Card from './components/Card-curso/Card'
 import Banner from './components/Banner-blackfriday/Banner'
+import Footer from './components/Footer/Footer'
 
 
 createRoot(document.getElementById('root')).render(
@@ -11,5 +12,6 @@ createRoot(document.getElementById('root')).render(
     <Header/>
     <Banner/>
     <Card preco={"90,00"}/>
+    <Footer/>
   </StrictMode>,
 )
