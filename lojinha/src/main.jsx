26bf383db81bd054f27dbaa2014349c97b87b29e
Gabriel Ribeiro <146293875+gmribeiro@users.noreply.github.com>
2024-11-19 20:56:02 -0300

    footer

--- lojinha/src/main.jsx
+++ lojinha/src/main.jsx
@@ -3,13 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './components/Header/Header'
 import Banner from './components/Banner-blackfriday/Banner'
-<<<<<<< HEAD
-
 import Cursos from './components/Cursos/Cursos'
 import Footer from './components/Footer/Footer'
-=======
 import Cursos from './components/Cursos/Cursos'
->>>>>>> 88d8a79e4a6dfb6634313d874ce175e26920d0a3
+
 
 
 createRoot(document.getElementById('root')).render(
@@ -17,9 +14,6 @@ createRoot(document.getElementById('root')).render(
     <Header/>
     <Banner/>
     <Cursos/>
-<<<<<<< HEAD
     <Footer/>
-=======
->>>>>>> 88d8a79e4a6dfb6634313d874ce175e26920d0a3
   </StrictMode>,
 )
