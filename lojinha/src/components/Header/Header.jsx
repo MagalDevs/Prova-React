5954bae746bb6b4275799e2e7363e79c5e387983
MagalDevs <danielgalfla12@gmail.com>
2024-11-19 19:39:14 -0300

    header feito

--- lojinha/src/components/Header/Header.jsx
+++ lojinha/src/components/Header/Header.jsx
@@ -1,22 +1,28 @@
 import React from 'react'
 import './Header.css'
 import { VscAccount } from "react-icons/vsc";
-import { IconContext } from "react-icons";
-
-
 
 const Header = () => {
   return (
     <header>
-        <img src="./imgs/2-removebg-preview.png" alt="" />
-        <input type="search" name="search" className='search' placeholder='Search'/>
-        
-        <div className='div-cadastro'>
-            <IconContext.Provider  value={{className: "icone"}}>
-                <VscAccount />
-            </IconContext.Provider>
-            
-            <a className='cadastro' href="#">Cadastre-se</a>
+        <div className="cima">
+          <img src="./imgs/2-removebg-preview.png" alt="" />
+          <input type="search" name="search" className='search' placeholder='Search'/>
+          
+          <div className='div-cadastro'>
+              <VscAccount className='icone'/>
+              <a className='cadastro' href="#">Cadastre-se</a>
+          </div>
+        </div>
+        <nav className='links'>
+          <a href="#">Home</a>
+          <a href="#">Produtos</a>
+          <a href="#">Sobre Nós</a>
+        </nav>
+        <div className='headline'> 
+          <p>Os Melhores Preços da Região!!!</p>
+          <p>Os Melhores Preços da Região!!!</p>
+          <p>Os Melhores Preços da Região!!!</p>
         </div>
     </header>
   )
