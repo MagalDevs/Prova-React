25c45f797f417170a665968f08993ce93c5eba7e
Gabriel Ribeiro <146293875+gmribeiro@users.noreply.github.com>
2024-11-20 14:45:56 -0300

    produtos inicio

---
+++ lojinha/src/components/Faixaprodutos/Faixaproduto.jsx
@@ -0,0 +1,26 @@
+import './faixaproduto.css'
+
+const faixaproduto = () => {
+  return (
+    <div className='todosprodutos'>
+      <div className='produtos'>
+          <h1>Basquetebol</h1>
+          
+      </div>
+      <div className='produtos'>
+          <h1>Futebol</h1>
+          
+      </div>
+      <div className='produtos'>
+          <h1>Tênis</h1>
+          
+      </div>
+      <div className='produtos'>
+          <h1>Natação</h1>
+          
+      </div>
+    </div>
+  )
+}
+
+export default faixaproduto
--- lojinha/src/pages/Produtos.jsx
+++ lojinha/src/pages/Produtos.jsx
@@ -1,10 +1,13 @@
 import './css/global.css'
+import Faixaproduto from '../components/Faixaprodutos/Faixaproduto'
 
 const Produtos = () => {
   return (
     <>
     <h1>Nossos Produtos</h1>
+    <Faixaproduto/>
     </>
+    
   )
 }
 
